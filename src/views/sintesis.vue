a4a2212a8a3f97940fe84f31f6290d2f3dac9103
Carlos Andres Diaz Pinto <98177573+AndresDiazPinto@users.noreply.github.com>
2026-02-25 08:26:23 -0500

    Update sintesis.vue

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -9,7 +9,7 @@
     .row.justify-content-center
       .col-lg-10.mb-5
         figure
-          img(src="@/assets/curso/temas/sintesis.svg", alt="alt")
+          img(src="@/assets/curso/temas/sintesis.svg", alt="Bases de datos Las bases de datos son: SQL y NoSQL. 1. SQL: es un lenguaje de consulta estructurado. Los gestores son: ORACLE, SQL, Mysql y MariaDB. Sus características: a. Conformadas por tablas. b. Relacionales entre tablas. c. Solvencia y consistencia en el almacenamiento y consulta de datos. 2. NoSQL: es paradigma para la administración de BD. Los gestores son: MongoDB, Redis y Cassandra. Sus características son:  a. Los datos no se almacenan en tablas. b. Arquitectura distribuida. c. Eficiencia en el procesamiento de datos. d. Su almacenamiento se hace en documentos.")
       .col-auto
         a.anexo.mb-4(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
           .anexo__icono
